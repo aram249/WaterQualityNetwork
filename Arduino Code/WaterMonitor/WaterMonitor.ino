654d06a4c1b23fcaf9267e255075def65ff39676
lcolias <lpcolias@gmail.com>
2020-12-20 15:00:45 -0800

    RTC/Timestamp added

--- Arduino Code/WaterMonitor/WaterMonitor.ino
+++ Arduino Code/WaterMonitor/WaterMonitor.ino
@@ -73,7 +73,7 @@ int connectionStatus;                   // CLIENTS CONNECTION STATUS
 //---SENSOR VARIABLES----------------------------------------------------------------------------------------------
 // Sensor Pins 
 #define DS18S20_Pin 2                   // DIGITAL WIRE 2 - DS18S20
-#define TURBIDITY_PIN A0                // ANALOG PIN 0 - TURBIDITY 
+#define TURBIDITY_PIN A2                // ANALOG PIN 2 - TURBIDITY 
 ///#define EC_PIN A1                       // ANALOG PIN 1 - ELECTIRCAL CONDUCTIVITY
 ///#define PH_PIN A0                       // ANALOG PIN 2 - PH
 
@@ -153,8 +153,9 @@ void setup() {
 	///ec.begin();
   ///ph.begin();
   
-  ///rtc.setup();
-  ///rtc.adjustRtc(F(__DATE__), F(__TIME__));
+  rtc.setup();
+  //Set the RTC time manually
+  //rtc.adjustRtc(2020,12,20,50,2,35,30);
   ///sdService.setup();
   ///sdService.update();
 }
@@ -167,14 +168,14 @@ void loop() {
 	{
 		updateTime = millis();
 
-    ///rtc.read();
+    rtc.read();
 
     // Collects DS18B120a temprature sensor reading(Displays to Serial Monitor for Debugging)
     tSensors.requestTemperatures(); 
     tempC = tSensors.getTempCByIndex(0);
         
     /// NEXT ITERATION WILL COLLECT THESE OTHER SENSORS DATA
-    ///turbidityVoltage = analogRead(A0) * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+    turbidityVoltage = analogRead(TURBIDITY_PIN) * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
     ///turbidityValue = getTurbidity(turbidityVoltage);// read the input on analog pin 0:    
     
     ///ecVoltage = analogRead(EC_PIN)/1024.0*5000;  // read the voltage
@@ -183,13 +184,16 @@ void loop() {
     ///pHVoltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
     ///phValue = ph.readPH(pHVoltage,temperature);  // convert voltage to pH with temperature compensation
 
-    ///timestamp = (String)rtc.month + "," + (String)rtc.day + "," + (String)rtc.year + " " + (String)rtc.hour + ":" + (String)rtc.minute + ":" + (String)rtc.second;
+    //Clear the JSON doc before inserting values
+    doc.clear();     
+
+    timestamp = (String)rtc.month + "," + (String)rtc.day + "," + (String)rtc.year + " " + (String)rtc.hour + ":" + (String)rtc.minute + ":" + (String)rtc.second;
     ///Serial.print(F("ºC , EC = ")); Serial.print(ecValue);
     ///Serial.print(F("ms/cm , pH = ")); Serial.print(phValue);
     ///Serial.print(F(" , Turbidity = ")); Serial.print(turbidityVoltage); Serial.println("NTU"); 
-    Serial.print(tempC); Serial.print("ºC");
-        
-    ///doc["timestamp"] = timestamp;
+    Serial.print(tempC); Serial.print("ºC, "); 
+    Serial.print(timestamp); 
+    doc["timestamp"] = timestamp;
     ///doc["Turbidity"] = turbidityValue;
     ///doc["pH"] = phValue;
     ///doc["EC"] = ecValue;
