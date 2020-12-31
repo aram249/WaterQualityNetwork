944091cc9d9b4a7eccfb1ca64495ca8e5f77b766
lcolias <lpcolias@gmail.com>
2020-12-30 21:30:58 -0800

    MQTt updates

--- Arduino Code/WaterMonitor/WaterMonitor.ino
+++ Arduino Code/WaterMonitor/WaterMonitor.ino
@@ -42,6 +42,9 @@
  * version :  V1.0
  * date    :  2020-10-06
  **********************************************************************/
+
+#include <PubSubClient.h>
+
 #include "GravityRtc.h"
 ///#include "SdService.h"
 
@@ -63,19 +66,25 @@ char ssid[] = "ATT5F7y3i8";        // YOUR WIFI SSID
 char password[] = "2u9+4d4d2?9x";  // YOUR WIFI PASSWORD
 
 // Server Information Variables 
-const char* server = "34.69.136.100";   // IP ADDRESS FOR THE CLOUD SERVER
-const int port = 54000;                 // PORT CONNECTION NUMBER
+char* hostname = "3.137.142.189"; // IP ADDRESS FOR THE SERVER
+int port = 1883;                 // PORT CONNECTION NUMBER
 
-// Client Insrtance and Information Variables
-WiFiClient client;                      // CLIENT INSTANCE THAT CONNECTS THE ARDUINO TO THE SERVER
-int connectionStatus;                   // CLIENTS CONNECTION STATUS
+// MQTT Client Variables
+WiFiClient c;                           // CLIENT INSTANCE THAT CONNECTS THE ARDUINO TO THE SERVER
+int connectionStatus = WL_IDLE_STATUS;  // CLIENT CONNECTION STATUS
+PubSubClient client(c);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (256)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+const char* topic = "WaterQuality";
 
 //---SENSOR VARIABLES----------------------------------------------------------------------------------------------
 // Sensor Pins 
 #define DS18S20_Pin 2                   // DIGITAL WIRE 2 - DS18S20
 #define TURBIDITY_PIN A2                // ANALOG PIN 2 - TURBIDITY 
-///#define EC_PIN A1                       // ANALOG PIN 1 - ELECTIRCAL CONDUCTIVITY
-///#define PH_PIN A0                       // ANALOG PIN 2 - PH
+#define EC_PIN A1                       // ANALOG PIN 1 - ELECTIRCAL CONDUCTIVITY
+#define PH_PIN A0                       // ANALOG PIN 2 - PH
 
 // Turbidity sensor variables
 float turbidityVoltage, turbidityValue;
@@ -102,6 +111,41 @@ unsigned long updateTime = 0;
 const size_t capacity = JSON_OBJECT_SIZE(5);
 StaticJsonDocument<capacity> doc;
 
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 //---SETUP---------------------------------------------------------------------------------------------------------
 void setup() {
@@ -131,27 +175,33 @@ void setup() {
   delay(1000); 
 
 //---CLIENT/SERVER-------------------------------------------------------------------------------------------------
+
+  client.setServer(hostname, port);
+  client.setCallback(callback);
+
+  //connect();
+  
   // Begins a client connection to the server
-  Serial.print("\n[Connecting to %s ... "); Serial.println(server);
-  connectionStatus = client.connect(server, port);
-  delay(1000);
+  //Serial.print("\n[Connecting to %s ... "); Serial.println(server);
+  //connectionStatus = client.connect(server, port);
+  //delay(1000);
   
   // If the client DOES connect then a confirmation message is printed and sent to the server. 
   // If the client DOES NOT connect then an errror messaage is printed with the conenction status
-  if(connectionStatus)
-  {
-    Serial.println("Connection to Server completed...");
-  }
-  else
-  {
-    Serial.print(F("Client unavailable, STATUS: ")); Serial.println(connectionStatus);
-  }
+  //if(connectionStatus)
+  //{
+  //  Serial.println("Connection to Server completed...");
+  //}
+  //else
+  //{
+  //  Serial.print(F("Client unavailable, STATUS: ")); Serial.println(connectionStatus);
+  //}
   
   // Start the DS18B20 sensor
   tSensors.begin();
  
-	///ec.begin();
-  ///ph.begin();
+	ec.begin();
+  ph.begin();
   
   rtc.setup();
   //Set the RTC time manually
@@ -164,7 +214,7 @@ void loop() {
 	
   
 	// ************************* Serial debugging ******************
-	if(millis() - updateTime > 5000)
+	if(millis() - updateTime > 10000)
 	{
 		updateTime = millis();
 
@@ -178,11 +228,11 @@ void loop() {
     turbidityVoltage = analogRead(TURBIDITY_PIN) * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
     turbidityValue = getTurbidity(turbidityVoltage);// read the input on analog pin 2:    
     
-    ///ecVoltage = analogRead(EC_PIN)/1024.0*5000;  // read the voltage
-    ///ecValue =  ec.readEC(ecVoltage,temperature); // convert voltage to EC with temperature compensation
+    ecVoltage = analogRead(EC_PIN)/1024.0*5000;  // read the voltage
+    ecValue =  ec.readEC(ecVoltage,tempC); // convert voltage to EC with temperature compensation
     
-    ///pHVoltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
-    ///phValue = ph.readPH(pHVoltage,temperature);  // convert voltage to pH with temperature compensation
+    pHVoltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
+    phValue = ph.readPH(pHVoltage,tempC);  // convert voltage to pH with temperature compensation
 
     //Clear the JSON doc before inserting values
     doc.clear();     
@@ -190,17 +240,43 @@ void loop() {
     timestamp = (String)rtc.month + "," + (String)rtc.day + "," + (String)rtc.year + " " + (String)rtc.hour + ":" + (String)rtc.minute + ":" + (String)rtc.second;
     ///Serial.print(F("ºC , EC = ")); Serial.print(ecValue);
     ///Serial.print(F("ms/cm , pH = ")); Serial.print(phValue);
-    Serial.print(F("Turb = ")); Serial.print(turbidityVoltage); Serial.println("NTU, "); 
-    Serial.print(F("Temp = ")); Serial.print(tempC); Serial.print("ºC, "); 
-    Serial.print(timestamp); 
-    doc["timestamp"] = timestamp;
-    doc["Turbidity"] = turbidityValue;
+    //Serial.print(F("Turb = ")); Serial.print(turbidityVoltage); Serial.println("NTU, "); 
+    //Serial.print(F("Temp = ")); Serial.print(tempC); Serial.print("ºC, "); 
+    //Serial.print(timestamp); 
+    //doc["timestamp"] = timestamp;
+    //doc["Turbidity"] = turbidityValue;
     ///doc["pH"] = phValue;
     ///doc["EC"] = ecValue;
-    doc["Temprature"] = tempC;
+    //doc["Temprature"] = tempC;
     
-    serializeJson(doc,Serial); Serial.print("\n");
+    //serializeJson(doc,Serial); Serial.print("\n");
+    /*
+    if (!client.isConnected())
+    {
+      connect();
+    }
+    else
+    {
+    // Send and receive QoS 0 message
+    char buf[1024]; 
+    String temp = "{\"Timestamp\":" + timestamp + ",\"Temperature\":" + (String)tempC + ",\"pH\":" + phValue + ",\"EC\":" + (String)ecValue + ",\"Turbidity\":" + (String)turbidityValue + "}";
+        
+    strcpy(buf, temp.c_str());
+    Serial.println(buf);
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    int rc = client.publish(topic, message);
+    if (rc != 1)
+    {
+      Serial.print("rc from client publish is ");
+      Serial.println(rc);
+    }
 
+    }
+    /*
     // If the Arduino(Client) is connected to the server then... 
     if (client.connected()) {
       
@@ -222,7 +298,21 @@ void loop() {
       delay(1000);
        
     }
-    
+    */
+
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+
+    String temp = "{\"Timestamp\":" + timestamp + ",\"Temperature\":" + (String)tempC + ",\"pH\":" + phValue + ",\"EC\":" + (String)ecValue + ",\"Turbidity\":" + (String)turbidityValue + "}";
+    ++value;
+    strcpy(msg, temp.c_str());
+    Serial.print("Publish message: ");
+    Serial.print(msg);
+    Serial.println(turbidityVoltage);
+    client.publish(topic, msg);
+  
     
 	}
  
@@ -230,23 +320,61 @@ void loop() {
   ///ph.calibration(pHVoltage,temperature);  // calibration process by Serial CMD 
 }
 
-// ************************************************************ time ********************** **********
-//Serial.print("   Year = ");//year
-//Serial.print(rtc.year);
-//Serial.print("   Month = ");//month
-//Serial.print(rtc.month);
-//Serial.print("   Day = ");//day
-//Serial.print(rtc.day);
-//Serial.print("   Week = ");//week
-//Serial.print(rtc.week);
-//Serial.print("   Hour = ");//hour
-//Serial.print(rtc.hour);
-//Serial.print("   Minute = ");//minute
-//Serial.print(rtc.minute);
-//Serial.print("   Second = ");//second
-//Serial.println(rtc.second);
-
 //Converts the voltage to a unit of turbidity using a relationship function between the two
 float getTurbidity(float voltage){
-  return -1129.4*voltage*voltage + 5742.3*voltage - 4352.9;
+  return (-1120.4*voltage*voltage) + (5742.3*voltage) - 4352.9;
 }
+/*
+void connect()
+{
+  Serial.print("Connecting to ");
+  Serial.print(hostname);
+  Serial.print(":");
+  Serial.println(port);
+ 
+  int rc = ipstack.connect(hostname, port);
+  if (rc != 1)
+  {
+    Serial.print("rc from TCP connect is ");
+    Serial.println(rc);
+  }
+ 
+  Serial.println("MQTT connecting");
+  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
+  data.MQTTVersion = 3;
+  data.clientID.cstring = (char*)"arduino-test";
+  rc = client.connect(data);
+  if (rc != 0)
+  {
+    Serial.print("rc from MQTT connect is ");
+    Serial.println(rc);
+  }
+  Serial.println("MQTT connected");
+  
+  rc = client.subscribe(topic, MQTT::QOS2, messageArrived);   
+  if (rc != 0)
+  {
+    Serial.print("rc from MQTT subscribe is ");
+    Serial.println(rc);
+  }
+  Serial.println("MQTT subscribed");
+}
+
+void messageArrived(MQTT::MessageData& md)
+{
+  MQTT::Message &message = md.message;
+  
+  Serial.print("Message ");
+  Serial.print(++arrivedcount);
+  Serial.print(" arrived: qos ");
+  Serial.print(message.qos);
+  Serial.print(", retained ");
+  Serial.print(message.retained);
+  Serial.print(", dup ");
+  Serial.print(message.dup);                
+  Serial.print(", packetid ");
+  Serial.println(message.id);
+  Serial.print("Payload ");
+  Serial.println((char*)message.payload);
+}
+*/
